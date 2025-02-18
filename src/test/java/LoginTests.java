f9f5cef84d20ead9fb8dd43a860d11b8bad06e11
kavya-qaE <kavya.ilapavuluri@testpro.io>
2025-02-18 12:54:08 -0800

    homework-22 POM
    homework-22 solved.

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,11 +1,19 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pom.HomePage;
+import pom.LoginPage;
 
 public class LoginTests extends BaseTest {
     @Test
 
     public void loginEmptyEmailPassword() {
-        provideEmail("kavya.ilapavuluri@testpro.io");
-        providePassword("student#67");
-        clickSubmit();
+        LoginPage loginPage=new LoginPage(driver);
+        HomePage homePage=new HomePage(driver);
+
+        loginPage.provideEmail("kavya.ilapavuluri@testpro.io");
+        loginPage.providePassword("student#67");
+        loginPage.clickSubmit();
+
+        Assert.assertEquals("https://qa.koel.app/",homePage.currentUrl());
     }
 }
