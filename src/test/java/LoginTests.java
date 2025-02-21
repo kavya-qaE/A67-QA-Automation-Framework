00bd574b5780175af6d65476344768e05a3a2bb0
kavya-qaE <kavya.ilapavuluri@testpro.io>
2025-02-21 12:15:06 -0800

    homework-23 PageFactory
    homework-23 solved.

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -2,17 +2,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pom.HomePage;
 import pom.LoginPage;
+import pom.LoginPageFactory;
 
 public class LoginTests extends BaseTest {
     @Test
 
     public void loginEmptyEmailPassword() {
-        LoginPage loginPage=new LoginPage(driver);
+        //LoginPage loginPage=new LoginPage(driver);
         HomePage homePage=new HomePage(driver);
+        //loginPage.provideEmail("kavya.ilapavuluri@testpro.io");
+        //loginPage.providePassword("student#67");
+        //loginPage.clickSubmit();
 
-        loginPage.provideEmail("kavya.ilapavuluri@testpro.io");
-        loginPage.providePassword("student#67");
-        loginPage.clickSubmit();
+        LoginPageFactory loginPageFactory=new LoginPageFactory(driver);
+        loginPageFactory.provideEmail("kavya.ilapavuluri@testpro.io");
+        loginPageFactory.providePassword("student#67");
+        loginPageFactory.clickSubmit();
+
 
         Assert.assertEquals("https://qa.koel.app/",homePage.currentUrl());
     }
